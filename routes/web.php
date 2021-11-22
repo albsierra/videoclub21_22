a78f726de4c6ebea88c3dc5e0c8ac4047f3d328f
alumno <alumno@vm1>
2021-11-22 08:08:01 +0100

    Solución ejercicios 2

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\CatalogController;
+
 Route::get('/', [HomeController::class, 'getHome']);
 
 Route::get('/catalog', [CatalogController::class, 'getIndex']);
