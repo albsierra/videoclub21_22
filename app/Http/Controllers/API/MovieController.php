645eeaf0a256504c26679fc4ca5408eb19b1cb37
alumno <alumno@vm1>
2022-01-25 12:16:48 +0100

    API sin terminar

---
+++ app/Http/Controllers/API/MovieController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Http;
+use App\Models\Movie;
+use Illuminate\Http\Request;
+use App\Http\Resources\MovieResource;
+
+class MovieController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return MovieResource::collection(Movie::paginate());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $movie = json_decode($request->getContent(), true);
+
+        $movie = movie::create($movie);
+
+        return new MovieResource($movie);
+    }
+
+    public function search ($search) {
+
+        $host = 'www.omdbapi.com';
+        $response = Http::get('http://' . $host . '/', [
+            'apikey' => env('OMDBAPI_KEY'),
+            's' => $search,
+            'page' => 1,
+            'r' => 'json'
+        ]);
+
+        return $response;
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Movie  $movie
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Movie $movie)
+    {
+        return new MovieResource($movie);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Movie  $movie
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Movie $movie)
+    {
+        $movieData = json_decode($request->getContent(), true);
+        $movie->update($movieData);
+
+        return new MovieResource($movie);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Movie  $movie
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Movie $movie)
+    {
+        $movie->delete();
+    }
+}
