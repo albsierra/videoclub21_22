6e1a330027273b1950eb836f9d16fe7b326af9b5
alumno <alumno@vm1>
2022-01-25 20:19:18 +0100

    Creando API

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
         self::seedCatalog();
         $this->command->info('Tabla catálogo inicializada con datos!');
     }
