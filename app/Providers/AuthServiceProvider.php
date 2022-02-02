643f9e157944b6ab5bb15c328460c244fdf3bc8f
alumno <alumno@vm1>
2022-02-02 09:55:22 +0100

    Autorizando videoclub con policies primeros tres puntos

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,9 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use App\Policies\MoviePolicy;
+use App\Models\Movie;
+use App\Models\User;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -14,6 +17,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+        Movie::class => MoviePolicy::class,
     ];
 
     /**
